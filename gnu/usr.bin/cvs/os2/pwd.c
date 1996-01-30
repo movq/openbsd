54eef230ec561512ad1c476c6b7bfd4a8930dfdd
tholo <tholo@openbsd.org>
1996-01-30 00:19:40 +0000

    Upgrade to 1.7.1 snapshot

--- gnu/usr.bin/cvs/os2/pwd.c
+++ gnu/usr.bin/cvs/os2/pwd.c
@@ -15,7 +15,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-    $Header: /home/mike/src/cvs/openbsd/src/gnu/usr.bin/cvs/os2/pwd.c,v 1.1 1996/01/30 00:19:38 tholo Exp $
+    $Header: /home/mike/src/cvs/openbsd/src/gnu/usr.bin/cvs/os2/pwd.c,v 1.1.1.1 1996/01/30 00:19:38 tholo Exp $
 */
 
 /* This 'implementation' is conjectured from the use of this functions in
