bf338622c1a1b928cca51bf670c5329b7133bd0c
peter <peter@openbsd.org>
1998-07-21 13:19:50 +0000

    Complete initial import from mySQL 3.22.4 (mit-pthreads/). Lots of dross to move and remove yet.
    At minimum:
    o	remove GNU config and GNU Makefiles
    o	build arch directory and migrate away machdep/
    o	rebuild BSD Makefiles
    o	move notes etc. into doc/

--- lib/libpthread/pthreads/_exit.c
+++ lib/libpthread/pthreads/_exit.c
@@ -36,7 +36,7 @@
  */
 
 #ifndef lint
-static const char rcsid[] = "$Id: _exit.c,v 1.1 1998/07/21 13:19:50 peter Exp $";
+static const char rcsid[] = "$Id: _exit.c,v 1.1.1.1 1998/07/21 13:19:50 peter Exp $";
 #endif
 
 #include <pthread.h>
--- lib/libpthread/pthreads/cleanup.c
+++ lib/libpthread/pthreads/cleanup.c
@@ -36,7 +36,7 @@
  */
 
 #ifndef lint
-static const char rcsid[] = "$Id: cleanup.c,v 1.1 1998/07/21 13:19:50 peter Exp $";
+static const char rcsid[] = "$Id: cleanup.c,v 1.1.1.1 1998/07/21 13:19:50 peter Exp $";
 #endif
 
 #include <pthread.h>
