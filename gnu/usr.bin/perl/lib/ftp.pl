ba48696e32e4f26f6579f5f7477bef9cc3c74d82
millert <millert@openbsd.org>
2000-04-06 16:11:10 +0000

    virgin perl 5.6.0

--- gnu/usr.bin/perl/lib/ftp.pl
+++ gnu/usr.bin/perl/lib/ftp.pl
@@ -1,14 +1,23 @@
 #-*-perl-*-
+#
+# This library is no longer being maintained, and is included for backward
+# compatibility with Perl 4 programs which may require it.
+#
+# In particular, this should not be used as an example of modern Perl
+# programming techniques.
+#
+# Suggested alternative: Net::FTP
+#
 # This is a wrapper to the chat2.pl routines that make life easier
 # to do ftp type work.
 # Mostly by Lee McLoughlin <lmjm@doc.ic.ac.uk>
 # based on original version by Alan R. Martello <al@ee.pitt.edu>
 # And by A.Macpherson@bnr.co.uk for multi-homed hosts
 #
-# $Header: /home/mike/src/cvs/openbsd/src/gnu/usr.bin/perl/lib/Attic/ftp.pl,v 1.1.1.2 1999/04/29 22:39:52 millert Exp $
+# $Header: /home/mike/src/cvs/openbsd/src/gnu/usr.bin/perl/lib/Attic/ftp.pl,v 1.1.1.3 2000/04/06 16:09:37 millert Exp $
 # $Log: ftp.pl,v $
-# Revision 1.1.1.2  1999/04/29 22:39:52  millert
-# perl5.005_03
+# Revision 1.1.1.3  2000/04/06 16:09:37  millert
+# virgin perl 5.6.0
 #
 # Revision 1.17  1993/04/21  10:06:54  lmjm
 # Send all status reports to STDERR not to STDOUT (to allow use by ftpcat).
