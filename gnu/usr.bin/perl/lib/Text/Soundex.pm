f26286d24aaf2ab36f3b06c5261d64e86f0c99e0
millert <millert@openbsd.org>
2001-05-24 18:26:23 +0000

    stock perl 5.6.1

--- gnu/usr.bin/perl/lib/Text/Soundex.pm
+++ gnu/usr.bin/perl/lib/Text/Soundex.pm
@@ -7,7 +7,7 @@ require Exporter;
 
 $VERSION = '1.0';
 
-# $Id: Soundex.pm,v 1.1.1.3 2000/04/06 16:09:50 millert Exp $
+# $Id: Soundex.pm,v 1.1.1.4 2001/05/24 18:23:38 millert Exp $
 #
 # Implementation of soundex algorithm as described by Knuth in volume
 # 3 of The Art of Computer Programming, with ideas stolen from Ian
@@ -25,8 +25,8 @@ $VERSION = '1.0';
 # Lukasiewicz, Lissajous -> L222
 #
 # $Log: Soundex.pm,v $
-# Revision 1.1.1.3  2000/04/06 16:09:50  millert
-# virgin perl 5.6.0
+# Revision 1.1.1.4  2001/05/24 18:23:38  millert
+# stock perl 5.6.1
 #
 # Revision 1.2  1994/03/24  00:30:27  mike
 # Subtle bug (any excuse :-) spotted by Rich Pinder <rpinder@hsc.usc.edu>
@@ -111,7 +111,7 @@ many people seem to prefer an I<unlikely> value like C<Z000>
 can be assigned to C<$soundex_nocode>.
 
 In scalar context C<soundex> returns the soundex code of its first
-argument, and in array context a list is returned in which each element is the 
+argument, and in list context a list is returned in which each element is the 
 soundex code for the corresponding argument passed to C<soundex> e.g.
 
   @codes = soundex qw(Mike Stok);
