1f3989e66a9582bb1541384a5397849aace009e3
cvs2git <cvs2git@openbsd.org>
2006-11-01 20:03:34 +0000

    Initialize CVS branch OPENBSD_4_0
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- usr.sbin/syslogd/syslogd.c
+++ usr.sbin/syslogd/syslogd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: syslogd.c,v 1.93 2006/09/17 18:28:34 djm Exp $	*/
+/*	$OpenBSD: syslogd.c,v 1.92 2005/06/10 01:41:43 millert Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -39,7 +39,7 @@ static const char copyright[] =
 #if 0
 static const char sccsid[] = "@(#)syslogd.c	8.3 (Berkeley) 4/4/94";
 #else
-static const char rcsid[] = "$OpenBSD: syslogd.c,v 1.93 2006/09/17 18:28:34 djm Exp $";
+static const char rcsid[] = "$OpenBSD: syslogd.c,v 1.92 2005/06/10 01:41:43 millert Exp $";
 #endif
 #endif /* not lint */
 
@@ -330,14 +330,13 @@ main(int argc, char *argv[])
 	if (Debug)
 		setlinebuf(stdout);
 
-	if ((fd = nullfd = open(_PATH_DEVNULL, O_RDWR)) == -1) {
+	if ((nullfd = open(_PATH_DEVNULL, O_RDWR)) == -1) {
 		logerror("Couldn't open /dev/null");
 		die(0);
 	}
-	while (fd++ <= 2) {
-		if (fcntl(fd, F_GETFL, 0) == -1)
-			if (dup2(nullfd, fd) == -1)
-				logerror("dup2");
+	while (nullfd < 2) {
+		dup2(nullfd, nullfd + 1);
+		nullfd++;
 	}
 
 	consfile.f_type = F_CONSOLE;
