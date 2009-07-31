defe292b51cfadefd99da4d5c285040003b6934b
miod <miod@openbsd.org>
2009-07-31 09:28:41 +0000

    By popular demand and peer pressure, check-in work in progress work to support the Yeelong Lemote mips-based netbook. Kernel bits only for now, needs polishing; most of this work done during h2k9 last month, although the porting effort started earlier this year.

--- sys/arch/loongson/include/db_machdep.h
+++ sys/arch/loongson/include/db_machdep.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: db_machdep.h,v 1.1 2009/07/31 09:26:25 miod Exp $ */
+/* $OpenBSD: db_machdep.h,v 1.1.1.1 2009/07/31 09:26:25 miod Exp $ */
 /* public domain */
 #include <mips64/db_machdep.h>
--- sys/arch/loongson/include/kcore.h
+++ sys/arch/loongson/include/kcore.h
@@ -1,3 +1,3 @@
-/* $OpenBSD: kcore.h,v 1.1 2009/07/31 09:26:25 miod Exp $ */
+/* $OpenBSD: kcore.h,v 1.1.1.1 2009/07/31 09:26:25 miod Exp $ */
 /* public domain */
 #include <mips64/kcore.h>
