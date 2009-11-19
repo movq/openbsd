95c4daf7269f6f063124a1f8991d91032f3d91de
miod <miod@openbsd.org>
2009-11-19 19:23:35 +0000

    By popular demand and peer pressure, check-in work in progress work to support the Yeelong Lemote mips-based netbook. Kernel bits only for now, needs polishing; most of this work done during h2k9 last month, although the porting effort started earlier this year.

--- sys/arch/loongson/loongson/mutex.c
+++ sys/arch/loongson/loongson/mutex.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mutex.c,v 1.1 2009/11/19 19:23:35 miod Exp $	*/
+/*	$OpenBSD: mutex.c,v 1.1.1.1 2009/11/19 19:23:35 miod Exp $	*/
 
 /*
  * Copyright (c) 2004 Artur Grabowski <art@openbsd.org>
