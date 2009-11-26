e666787eb43b2d1da0510b72f5b5f2b303f1ce34
miod <miod@openbsd.org>
2009-11-26 12:14:01 +0000

    By popular demand and peer pressure, check-in work in progress work to support the Yeelong Lemote mips-based netbook. Kernel bits only for now, needs polishing; most of this work done during h2k9 last month, although the porting effort started earlier this year.

--- sys/arch/loongson/dev/mcclockvar.h
+++ sys/arch/loongson/dev/mcclockvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mcclockvar.h,v 1.1 2009/11/26 12:14:01 miod Exp $	*/
+/*	$OpenBSD: mcclockvar.h,v 1.1.1.1 2009/11/26 12:14:01 miod Exp $	*/
 /*	$NetBSD: mcclockvar.h,v 1.2 1996/04/17 22:22:38 cgd Exp $	*/
 
 /*
