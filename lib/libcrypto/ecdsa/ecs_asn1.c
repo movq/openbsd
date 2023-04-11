350be3f39c777ed654072e7cc746bbf3f8206573
cvs2git <cvs2git@openbsd.org>
2023-04-11 15:45:39 +0000

    Initialize CVS branch OPENBSD_7_3
    The per-file CVS branch roots do not exactly match a reconstructed source-branch commit.

--- lib/libcrypto/ecdsa/ecs_asn1.c
+++ lib/libcrypto/ecdsa/ecs_asn1.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ecs_asn1.c,v 1.14 2023/03/25 09:09:28 tb Exp $ */
+/* $OpenBSD: ecs_asn1.c,v 1.13 2023/03/07 09:27:10 jsing Exp $ */
 /* ====================================================================
  * Copyright (c) 2000-2002 The OpenSSL Project.  All rights reserved.
  *
@@ -53,11 +53,9 @@
  *
  */
 
-#include <openssl/asn1t.h>
-#include <openssl/bn.h>
-#include <openssl/err.h>
-
 #include "ecs_local.h"
+#include <openssl/err.h>
+#include <openssl/asn1t.h>
 
 static const ASN1_TEMPLATE ECDSA_SIG_seq_tt[] = {
 	{
