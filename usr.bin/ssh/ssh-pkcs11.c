62a1c3525cd1a881e08ba944c3fffb61962556f3
bluhm <bluhm@openbsd.org>
2023-07-19 14:08:59 +0000

    Disallow remote addition of FIDO/PKCS11 provider libraries to ssh-agent by default.
    The old behaviour of allowing remote clients from loading providers
    can be restored using `ssh-agent -O allow-remote-pkcs11`.
    
    Detection of local/remote clients requires a ssh(1) that supports
    the `session-bind@openssh.com` extension. Forwarding access to a
    ssh-agent socket using non-OpenSSH tools may circumvent this control.
    
    from djm@; ok markus@
    
    terminate process if requested to load a PKCS#11 provider that
    isn't a PKCS#11 provider; from / ok markus@
    from djm@
    
    this is errata/7.2/032_ssh_agent.patch.sig

--- usr.bin/ssh/ssh-pkcs11.c
+++ usr.bin/ssh/ssh-pkcs11.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: ssh-pkcs11.c,v 1.55 2021/11/18 21:11:01 djm Exp $ */
+/* $OpenBSD: ssh-pkcs11.c,v 1.55.6.1 2023/07/19 14:08:59 bluhm Exp $ */
 /*
  * Copyright (c) 2010 Markus Friedl.  All rights reserved.
  * Copyright (c) 2014 Pedro Martelletto. All rights reserved.
@@ -1512,10 +1512,8 @@ pkcs11_register_provider(char *provider_id, char *pin,
 		error("dlopen %s failed: %s", provider_id, dlerror());
 		goto fail;
 	}
-	if ((getfunctionlist = dlsym(handle, "C_GetFunctionList")) == NULL) {
-		error("dlsym(C_GetFunctionList) failed: %s", dlerror());
-		goto fail;
-	}
+	if ((getfunctionlist = dlsym(handle, "C_GetFunctionList")) == NULL)
+		fatal("dlsym(C_GetFunctionList) failed: %s", dlerror());
 	p = xcalloc(1, sizeof(*p));
 	p->name = xstrdup(provider_id);
 	p->handle = handle;
