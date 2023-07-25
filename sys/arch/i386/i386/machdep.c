d0813ed1e0d9066a542301cc7e5af7b5bad6840c
bluhm <bluhm@openbsd.org>
2023-07-25 14:53:31 +0000

    Some hypervisors (such as Hertzner) allow msr read of DE_CFG (which does not indicate bit 9 set, but they could have a firmware fix) but then block a msr write to bit 9 (which disables enough AVX optimizations to prevent the exfiltration of data), with a fault.  So let's also check the HV bit before we decide to modify the bit.  hypervisors are expected to set that bit. tested by lucas at sexy dot is. from deraadt@; with jsg@; ok mlarkin@
    this is errata/7.2/037_hvamdcpu.patch.sig

--- sys/arch/i386/i386/machdep.c
+++ sys/arch/i386/i386/machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: machdep.c,v 1.656.2.1 2023/07/24 19:33:25 bluhm Exp $	*/
+/*	$OpenBSD: machdep.c,v 1.656.2.2 2023/07/25 14:53:31 bluhm Exp $	*/
 /*	$NetBSD: machdep.c,v 1.214 1996/11/10 03:16:17 thorpej Exp $	*/
 
 /*-
@@ -2015,7 +2015,8 @@ identifycpu(struct cpu_info *ci)
 			if (msr != nmsr)
 				wrmsr(MSR_DE_CFG, nmsr);
 		}
-		if (family == 0x17 && ci->ci_model >= 0x31) {
+		if (family == 0x17 && ci->ci_model >= 0x31 &&
+		    (cpu_ecxfeature & CPUIDECX_HV) == 0) {
 			nmsr = msr = rdmsr(MSR_DE_CFG);
 #define DE_CFG_SERIALIZE_9 (1 << 9)		/* Zenbleed chickenbit */
 			nmsr |= DE_CFG_SERIALIZE_9;
