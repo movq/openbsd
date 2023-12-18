fc501a439e41bc39101beaf8cbb2c065c9f66865
bluhm <bluhm@openbsd.org>
2023-12-18 14:57:43 +0000

    implement "strict key exchange" in ssh and sshd
    This adds a protocol extension to improve the integrity of the SSH
    transport protocol, particular in and around the initial key exchange
    (KEX) phase.
    
    Full details of the extension are in the PROTOCOL file.
    
    from djm@; with markus@
    
    this is errata/7.3/024_ssh.patch.sig

--- usr.bin/ssh/kex.h
+++ usr.bin/ssh/kex.h
@@ -1,4 +1,4 @@
-/* $OpenBSD: kex.h,v 1.118 2023/03/06 12:14:48 dtucker Exp $ */
+/* $OpenBSD: kex.h,v 1.118.4.1 2023/12/18 14:57:43 bluhm Exp $ */
 
 /*
  * Copyright (c) 2000, 2001 Markus Friedl.  All rights reserved.
@@ -141,6 +141,7 @@ struct kex {
 	u_int	kex_type;
 	char	*server_sig_algs;
 	int	ext_info_c;
+	int	kex_strict;
 	struct sshbuf *my;
 	struct sshbuf *peer;
 	struct sshbuf *client_version;
