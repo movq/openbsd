74d70f377dcd492dd6d4d08da096caf37873ec0b
bluhm <bluhm@openbsd.org>
2024-10-29 17:03:28 +0000

    fix ML-KEM768x25519 KEX on big-endian systems; from djm@; spotted by jsg@; feedback/ok deraadt@
    this is errata/7.6/003_ssh.patch.sig

--- usr.bin/ssh/kexmlkem768x25519.c
+++ usr.bin/ssh/kexmlkem768x25519.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: kexmlkem768x25519.c,v 1.1 2024/09/02 12:13:56 djm Exp $ */
+/* $OpenBSD: kexmlkem768x25519.c,v 1.1.2.1 2024/10/29 17:03:28 bluhm Exp $ */
 /*
  * Copyright (c) 2023 Markus Friedl.  All rights reserved.
  *
@@ -30,6 +30,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <signal.h>
+#include <endian.h>
 
 #include "sshkey.h"
 #include "kex.h"
