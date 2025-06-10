fd2c621c6e9d545b2204b405ad51f89754717f3c
bluhm <bluhm@openbsd.org>
2025-06-10 18:16:27 +0000

    The counter for halfopen states should be manipulated using atomic_ops(9f). In current code the counter may underflow. The issue has been noticed and reported by Pascal Stumpf (pascal _at_ stumpf _dot_ co). The change has been also kindly tested by Pascal.
    from sashan@; OK @bluhm, OK @dlg
    
    this is errata/7.6/017_pfsyncook.patch.sig

--- sys/net/pf.c
+++ sys/net/pf.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pf.c,v 1.1205 2024/09/04 07:54:52 mglocker Exp $ */
+/*	$OpenBSD: pf.c,v 1.1205.2.1 2025/06/10 18:16:27 bluhm Exp $ */
 
 /*
  * Copyright (c) 2001 Daniel Hartmeier
@@ -382,7 +382,7 @@ pf_set_protostate(struct pf_state *st, int which, u_int8_t newstate)
 	    !(TCPS_HAVEESTABLISHED(st->src.state) ||
 	    st->src.state == TCPS_CLOSED) &&
 	    (TCPS_HAVEESTABLISHED(newstate) || newstate == TCPS_CLOSED))
-		pf_status.states_halfopen--;
+		atomic_dec_int(&pf_status.states_halfopen);
 
 	st->src.state = newstate;
 }
@@ -4677,7 +4677,7 @@ pf_create_state(struct pf_pdesc *pd, struct pf_rule *r, struct pf_rule *a,
 		pf_set_protostate(st, PF_PEER_SRC, TCPS_SYN_SENT);
 		pf_set_protostate(st, PF_PEER_DST, TCPS_CLOSED);
 		st->timeout = PFTM_TCP_FIRST_PACKET;
-		pf_status.states_halfopen++;
+		atomic_inc_int(&pf_status.states_halfopen);
 		break;
 	case IPPROTO_UDP:
 		pf_set_protostate(st, PF_PEER_SRC, PFUDPS_SINGLE);
