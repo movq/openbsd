1fcfd1af9bb4288fbb187e0ba0624b5bf5e9dd58
bluhm <bluhm@openbsd.org>
2026-04-18 19:58:35 +0000

    Prevent buffer overflow by checking the correct counter.
    An attacker on the same layer 2 network can send rogue router
    advertisements, potentially crashing slaacd.
    
    from Maurice Hieronymus (mhi AT mailbox.org), thanks!
    from florian@; OK deraadt
    
    this is errata/7.7/039_slaacd.patch.sig

--- sbin/slaacd/engine.c
+++ sbin/slaacd/engine.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: engine.c,v 1.99.4.1 2026/04/13 15:50:39 bluhm Exp $	*/
+/*	$OpenBSD: engine.c,v 1.99.4.2 2026/04/18 19:58:35 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2017 Florian Obser <florian@openbsd.org>
@@ -2085,7 +2085,7 @@ update_iface_ra_rdns(struct slaacd_iface *iface, struct radv *ra)
 	LIST_FOREACH(radv_rdns, &ra->rdns_servers, entries) {
 		memcpy(&rdns[rdns_count++],
 		    &radv_rdns->rdns, sizeof(struct in6_addr));
-		if (rdns_proposal->rdns_count == MAX_RDNS_COUNT)
+		if (rdns_count == MAX_RDNS_COUNT)
 			break;
 	}
 
