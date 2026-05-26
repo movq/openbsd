aa07f321c8c7932f863e192a2b9033d39d1a6259
bluhm <bluhm@openbsd.org>
2026-05-26 19:04:54 +0000

    7.9-stable

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 #
-#	$OpenBSD: newvers.sh,v 1.215 2026/04/13 17:22:23 deraadt Exp $
+#	$OpenBSD: newvers.sh,v 1.215.2.1 2026/05/26 19:04:54 bluhm Exp $
 #	$NetBSD: newvers.sh,v 1.17.2.1 1995/10/12 05:17:11 jtc Exp $
 #
 # Copyright (c) 1984, 1986, 1990, 1993
@@ -71,11 +71,11 @@ ost="OpenBSD"
 osr="7.9"
 
 cat >vers.c <<eof
-#define STATUS ""			/* release */
+#define STATUS "-stable"		/* stable branch */
 #if 0
+#define STATUS ""			/* release */
 #define STATUS "-beta"			/* just before a release */
 #define STATUS "-current"		/* just after a release */
-#define STATUS "-stable"		/* stable branch */
 #endif
 
 const char ostype[] = "${ost}";
