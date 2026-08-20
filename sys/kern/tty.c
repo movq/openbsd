e3af5669717d26850508b7f765a55d441a2fcf57
bluhm <bluhm@openbsd.org>
2026-08-20 07:39:40 +0000

    TIOCGSID can dereference a freed t_session, because we are storing s_leader in the wrong place.  This is a cold hard crash crashes.  Some folk are looking at fixing this properly but it will take some time, so in the meantime we should just fail the ioctl.
    from deraadt@; from Acts1632, OK kettenis@
    
    this is errata/7.8/049_ttyioctl.patch.sig

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tty.c,v 1.182 2025/09/25 08:46:50 mvs Exp $	*/
+/*	$OpenBSD: tty.c,v 1.182.2.1 2026/08/20 07:39:40 bluhm Exp $	*/
 /*	$NetBSD: tty.c,v 1.68.4.2 1996/06/06 16:04:52 thorpej Exp $	*/
 
 /*-
@@ -844,6 +844,8 @@ ttioctl(struct tty *tp, u_long cmd, caddr_t data, int flag, struct proc *p)
 		break;
 	case TIOCGSID:			/* get sid of tty */
 		if (!isctty(pr, tp))
+			return (ENOTTY);
+		if (tp->t_session->s_leader == NULL)	/* XXX session stored wrong */
 			return (ENOTTY);
 		*(int *)data = tp->t_session->s_leader->ps_pid;
 		break;
