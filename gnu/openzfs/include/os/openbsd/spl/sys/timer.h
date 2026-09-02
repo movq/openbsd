cd8b9f27437f84ccceabb3114249bfca36496bd4
Mike Jones <mike@mjones.org>
2026-09-02 13:48:43 +0100

    zfs: spun-out lock

--- gnu/openzfs/include/os/openbsd/spl/sys/timer.h
+++ gnu/openzfs/include/os/openbsd/spl/sys/timer.h
@@ -20,7 +20,13 @@
 #define	ddi_time_after_eq64(a, b)	ddi_time_before_eq64((b), (a))
 
 void	zfs_usleep_range(unsigned long, unsigned long);
+void	zfs_delay(clock_t);
 
 #define	usleep_range(min, max)	zfs_usleep_range((min), (max))
+/* Solaris delay() sleeps in ticks; OpenBSD delay() busy-waits in usecs. */
+#ifdef delay
+#undef delay
+#endif
+#define	delay(ticks)		zfs_delay((ticks))
 
 #endif /* _SPL_SYS_TIMER_H */
