ea63e304e54ebf72638be49b957b028d9ec6bd95
Mike Jones <mike@mjones.org>
2026-09-02 18:53:22 +0100

    zfs: memory pressure benchmark

---
+++ regress/sys/uvm/zfs_pressure/mempressure.d
@@ -0,0 +1,38 @@
+mempressure.o: mempressure.c /usr/include/sys/types.h \
+  /usr/include/sys/cdefs.h /usr/include/machine/cdefs.h \
+  /usr/include/sys/endian.h /usr/include/sys/_endian.h \
+  /usr/include/sys/_types.h /usr/include/machine/_types.h \
+  /usr/include/machine/endian.h /usr/include/sys/mman.h \
+  /usr/include/err.h /usr/include/errno.h /usr/include/sys/errno.h \
+  /usr/include/signal.h /usr/include/sys/signal.h \
+  /usr/include/machine/signal.h /usr/include/sys/siginfo.h \
+  /usr/include/sys/time.h /usr/include/sys/select.h /usr/include/time.h \
+  /usr/include/sys/_null.h /usr/include/sys/_time.h \
+  /usr/include/stdint.h /usr/include/stdio.h /usr/include/stdlib.h \
+  /usr/include/unistd.h /usr/include/sys/unistd.h
+/usr/include/sys/types.h:
+/usr/include/sys/cdefs.h:
+/usr/include/machine/cdefs.h:
+/usr/include/sys/endian.h:
+/usr/include/sys/_endian.h:
+/usr/include/sys/_types.h:
+/usr/include/machine/_types.h:
+/usr/include/machine/endian.h:
+/usr/include/sys/mman.h:
+/usr/include/err.h:
+/usr/include/errno.h:
+/usr/include/sys/errno.h:
+/usr/include/signal.h:
+/usr/include/sys/signal.h:
+/usr/include/machine/signal.h:
+/usr/include/sys/siginfo.h:
+/usr/include/sys/time.h:
+/usr/include/sys/select.h:
+/usr/include/time.h:
+/usr/include/sys/_null.h:
+/usr/include/sys/_time.h:
+/usr/include/stdint.h:
+/usr/include/stdio.h:
+/usr/include/stdlib.h:
+/usr/include/unistd.h:
+/usr/include/sys/unistd.h:
